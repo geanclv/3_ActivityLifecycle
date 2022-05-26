8577301833f4040c261f03632f06f0b8c96a783f
geanclv <geanclv@gmail.com>
2022-05-26 17:15:38 -0500

    Creación de un BaseActivity para demostrar el ciclo de vida de los Activities (Main y Type heredan este BasicActivity). Ajustes básicos y formateo a los layout.

--- app/src/main/java/com/geancarloleiva/a3_activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/geancarloleiva/a3_activitylifecycle/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.geancarloleiva.a3_activitylifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BasicActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btnStart)
+        btnStart.setOnClickListener{
+            val typeIntent = Intent(this, TypeActivity::class.java)
+            startActivity(typeIntent)
+        }
     }
 }
